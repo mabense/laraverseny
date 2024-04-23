807fb4f522d409abe558e1cc2e165a886f757224
Bense Mate <h846858@stud.u-szeged.hu>
2024-04-23 21:11:59 +0200

    ajax navigation

--- public/js/ajax/nav.js
+++ public/js/ajax/nav.js
@@ -1,1 +1,57 @@
 // alert("AJAX");
+
+function updateSegments(result) {
+    $template = $('#ajax').prop('content');
+    $('title').html($($template).find('title').text() + result['title']);
+    $('#page').html(result['page']);
+    $('footer').html(result['feedback']);
+
+    navClose();
+}
+
+function navClose() {
+    $('aside').fadeOut();
+}
+
+$(document).ready(function () {
+
+
+    $("a").on('click', function (event) {
+        // AJAX navigation on links
+
+        event.preventDefault();
+        // event.stopPropagation();
+        // event.stopImmediatePropagation();
+
+        let state = {
+            url: this.href,
+            type: 'GET',
+            success: function (result) {
+                updateSegments(result);
+            }
+        };
+
+        history.pushState({}, '', this.href);
+
+        jQuery.ajax(state);
+    });
+
+
+    $(window).on('popstate', function(event) {
+        // AJAX navigation on browser's back button
+        
+        event.preventDefault();
+        // event.stopPropagation();
+        // event.stopImmediatePropagation();
+    
+        jQuery.ajax({
+            url: location.href,
+            type: 'GET',
+            success: function(result) {
+                updateSegments(result);
+            }
+        });
+    });
+
+
+});
