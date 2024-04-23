d4685abcf97251700f52149e3e015e4337cfef90
Bense Mate <h846858@stud.u-szeged.hu>
2024-04-23 18:49:48 +0200

    made card component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,13 +18,15 @@
     <link rel="stylesheet" href="{{ asset('/css/links.css') }}" />
     <link rel="stylesheet" href="{{ asset('/css/common.css') }}" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-    integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
     crossorigin="anonymous" referrerpolicy="no-referrer" ></script>
     <script src="{{ asset('/js/ajax/nav.js') }}" type="text/javascript" ></script>
 </head>
 
-<body>
-    @yield('page')
+<body class="container column">
+    <x-card class="centered stretch-height column">
+        @yield('page')
+    </x-card>
 </body>
 
 </html>
